21b5113c461bdb94c5946aba8f78bf441ce4717e
kano-e <till.cattail@gmail.com>
2015-08-15 17:36:51 +0900

    Add #give method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
   has_many :skill_sets
   has_many :gave_skill_sets
+
+  # あるユーザに対するスキル評価
+  #
+  # @param [User] target スキル評価対象ユーザ
+  # @param [Array<Skill>] skills 評価したスキル一覧
+  # @return [void]
+  def give(target, skills)
+    SkillSet.create(
+      user: target,
+      skills: skills,
+    )
+  end
 end
