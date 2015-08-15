307d4eb6093873f176b11b7602016f9459924d7b
kano-e <till.cattail@gmail.com>
2015-08-15 17:36:29 +0900

    foreign_key 設定で落ちるので修正

--- db/migrate/20150815081157_create_skill_sets.rb
+++ db/migrate/20150815081157_create_skill_sets.rb
@@ -2,7 +2,7 @@ class CreateSkillSets < ActiveRecord::Migration
   def change
     create_table :skill_sets do |t|
       t.references :user, index: true, foreign_key: true
-      t.references :created_by, index: true, foreign_key: true
+      t.references :created_by, index: true
       t.timestamps null: false
     end
   end
