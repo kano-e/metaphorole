52a85a6cdbb184c3f27131a4d2169f3e06d087cd
kano-e <till.cattail@gmail.com>
2015-08-18 20:29:47 +0900

    保存前後に能力値計算

--- app/models/skill_set.rb
+++ app/models/skill_set.rb
@@ -1,17 +1,27 @@
 class SkillSet < ActiveRecord::Base
   belongs_to :user
   belongs_to :created_by, class_name: 'User'
-  has_many :items, ->(record){ order(number: :asc) }, class_name: 'SkillSetItem'
+  has_many :items, class_name: 'SkillSetItem'
   has_many :skills, through: :items
 
-  after_create :call_evaluate_ability
+  before_save :evaluate_ability
+  after_save :call_evaluate_ability_for_user
 
   scope :mine, -> { where(user: arel_table[:created_by]) }
   scope :given, -> { where.not(user: arel_table[:created_by]) }
 
+  def evaluate_ability
+    abilities = %w(attack_point defence_point heal_point enchant_point)
+    columns = abilities.map{|name| Skill.arel_table[name].sum.as(name) }
+    result = Skill.select(columns).where(id: skills.map(&:id)).take
+
+    self.attributes = result.attributes.slice(*abilities) if result
+    true
+  end
+
   private
 
-  def call_evaluate_ability
+  def call_evaluate_ability_for_user
     user.call_evaluate_ability if user
   end
 end
