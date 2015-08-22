47cc3db98a318ba424495e61165f42d5ad9259f7
kano-e <till.cattail@gmail.com>
2015-08-22 14:39:01 +0900

    Add UserSessionsController#create

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,25 @@
+class UserSessionsController < ApplicationController
+  def create
+    account = Account.authenticate(env['omniauth.auth'])
+    if current_user && account.user
+      if current_user != account.user
+        raise # エラーは後で考える
+      end
+
+    elsif current_user
+      if current_user.accounts.by(account.provider)
+        raise # エラーは後で考える
+      end
+      current_user.accounts << account
+
+    elsif account.user
+      unless UserSession.create(account.user)
+        raise # エラーは後で考える
+      end
+
+    else
+      account.build_user
+      account.save!
+    end
+  end
+end
